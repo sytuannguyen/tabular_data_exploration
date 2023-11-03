8180d172e28c702381ee424a1c3209b6cba05a76
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2023-11-03 10:28:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def display_correlation_heatmap(data):
     figure_size = st.slider("Figure Size", min_value=5, max_value=20, value=10, step=1)
     numerical_cols = data.select_dtypes(include=['float64', 'int64'])
     corr_matrix = numerical_cols.corr()
-    plt.figure(figsize=figure_size)
+    plt.figure(figsize=(figure_size,figure_size))
     sns.heatmap(corr_matrix, annot=show_annotation, cmap='coolwarm', fmt=".2f", linewidths=.5)
     st.pyplot(plt)
 
@@ -64,7 +64,7 @@ def main():
     elif selected_option == "Display Basic Statistics":
         display_statistics(data)
     elif selected_option == "Display Correlation Heatmap":
-        display_correlation_heatmap(data, show_annotation, (figure_size, figure_size))
+        display_correlation_heatmap(data)
     elif selected_option == "Display Histogram":
         display_histogram(data)
 
