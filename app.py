5853e63c5e346792a942c197f63e815c8f6faf4e
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2023-11-03 09:51:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ def load_data(file):
     data = pd.read_csv(file)
     return data
 
-# Function to display basic info
-def display_info(data):
-    st.write("### Dataset Info")
-    st.write(data.info())
+# Function to display the dataset
+def display_dataframe(data):
+    st.write("### Show the data")
+    st.dataframe(data)
 
 # Function to display head of the dataset
 def display_head(data):
@@ -22,26 +22,6 @@ def display_head(data):
 def display_statistics(data):
     st.write("### Basic Statistics")
     st.write(data.describe())
-
-# Function to fill missing values
-def fill_missing_values(data):
-    st.write("### Fill Missing Values")
-    filled_data = data.fillna(method='ffill')  # You can use any filling method here
-    st.write(filled_data)
-
-# Function to plot histogram
-def plot_histogram(data, feature):
-    st.write("### Histogram Plot")
-    plt.figure(figsize=(8, 6))
-    sns.histplot(data[feature], kde=True)
-    st.pyplot()
-
-# Function to plot heatmap for feature correlation
-def plot_heatmap(data):
-    st.write("### Heatmap for Feature Correlation")
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(data.corr(), annot=True, cmap='coolwarm')
-    st.pyplot()
 
 # Main function
 def main():
@@ -54,24 +34,16 @@ def main():
         data = load_data(uploaded_file)
         
         # Dropdown for data exploration options
-        options = ["Display Info", "Display Head", "Display Basic Statistics", "Fill Missing Values", "Histogram Plot", "Heatmap Plot"]
+        options = ["Display Data", "Display Head", "Display Basic Statistics"]
         selected_option = st.selectbox("Select an option:", options)
 
         # Perform the selected action based on the dropdown choice
-        if selected_option == "Display Info":
-            #display_info(data)
-            st.dataframe(data)
+        if selected_option == "Display Data":
+            display_dataframe(data)
         elif selected_option == "Display Head":
             display_head(data)
         elif selected_option == "Display Basic Statistics":
             display_statistics(data)
-        elif selected_option == "Fill Missing Values":
-            fill_missing_values(data)
-        elif selected_option == "Histogram Plot":
-            selected_feature = st.selectbox("Select a feature for histogram plot:", data.columns)
-            plot_histogram(data, selected_feature)
-        elif selected_option == "Heatmap Plot":
-            plot_heatmap(data)
 
 # Run the app
 if __name__ == "__main__":
