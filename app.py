76969b4b765b325a279b4676a009fb3de67f2ee5
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2023-11-03 13:18:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,16 +31,18 @@ def fill_missing_values(data, fill_strategy):
 # Function to transform categorical columns to numerical using ordinal encoding or one-hot encoding
 def transform_categorical_data(data, categorical_cols, max_onehot_categories=10):
     transformed_data = data.copy()
-    st.write(categorical_cols)
-    '''for col in categorical_cols:
+    st.write('List of categorical columns:\n', categorical_cols)
+    for col in categorical_cols:
         if len(data[col].unique()) <= max_categories:
-            onehot_encoder = OneHotEncoder(sparse=False, drop='first')
+            st.write(col, str(data[col].unique()))
+            '''onehot_encoder = OneHotEncoder(sparse=False, drop='first')
             onehot_encoded = onehot_encoder.fit_transform(data[[col]])
             onehot_df = pd.DataFrame(onehot_encoded, columns=[f"{col}_{int(val)}" for val in onehot_encoder.categories_[0][1:]])
             transformed_data = pd.concat([transformed_data, onehot_df], axis=1)
-            transformed_data.drop(columns=[col], inplace=True)
+            transformed_data.drop(columns=[col], inplace=True)'''
         else:
-            ordinal_encoder = OrdinalEncoder()
+            st.write(col, str(data[col].unique()))
+            '''ordinal_encoder = OrdinalEncoder()
             transformed_data[col] = ordinal_encoder.fit_transform(data[col])'''
     
     return transformed_data
