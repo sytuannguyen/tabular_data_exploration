fb96c02656945fccf59686f72c0e084e529d7a8a
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2023-11-03 13:13:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def fill_missing_values(data, fill_strategy):
 def transform_categorical_data(data, categorical_cols, max_onehot_categories=10):
     transformed_data = data.copy()
 
-    '''for col in categorical_cols:
+    for col in categorical_cols:
         if len(data[col].unique()) <= max_categories:
             onehot_encoder = OneHotEncoder(sparse=False, drop='first')
             onehot_encoded = onehot_encoder.fit_transform(data[[col]])
@@ -41,7 +41,7 @@ def transform_categorical_data(data, categorical_cols, max_onehot_categories=10)
             transformed_data.drop(columns=[col], inplace=True)
         else:
             ordinal_encoder = OrdinalEncoder()
-            transformed_data[col] = ordinal_encoder.fit_transform(data[col])'''
+            transformed_data[col] = ordinal_encoder.fit_transform(data[col])
     
     return transformed_data
     
